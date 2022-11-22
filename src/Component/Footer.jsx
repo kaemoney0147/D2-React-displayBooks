94b02f037aa4e2db4eac2cce4371544e361b98a9
abiodun <k.akinlotan@yahoo.com>
2022-11-22 16:01:07 +0000

    gave footer some margin

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,23 +3,23 @@ const Footer=()=>{
     return(
         <div id='footesfluid'className=" bg-primary fluid">
         <Container className='mt-5 d-flex justify-content-between '>
-        <div className="footer">
-        <ul className="mt-5">
+        <div className="footer mt-5">
+        <ul>
             <li>Help</li>
             <li>Advisories</li>
             <li>Status</li>
             <li>Contact Bookshop</li>
         </ul>
         </div>
-        <div className="footer">
-        <ul className="mt-5">
+        <div className="footer mt-5">
+        <ul >
             <li>About</li>
             <li>Bloog</li>
             <li>Press</li>
         </ul>
         </div>
-        <div className="footer">
-        <ul className="mt-5">
+        <div className="footer mt-5">
+        <ul>
             <li>Policy</li>
             <li>Term of Use</li>
             <li>Status</li>
